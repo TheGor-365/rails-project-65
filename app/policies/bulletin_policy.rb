285398037b95da5231b98b9b62ce1aaa7a46effc
Gor <7495500@gmail.com>
2025-09-23 12:11:14 +0300

    Fix CI offiness 3

--- app/policies/bulletin_policy.rb
+++ app/policies/bulletin_policy.rb
@@ -1,19 +1,49 @@
 # frozen_string_literal: true
 
 class BulletinPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    record.state == 'published' || owner?
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    user.present?
+  end
+
+  # Нужно для GET /bulletins/:id/edit
+  def edit?
+    owner?
+  end
+
+  # Нужно для PATCH/PUT /bulletins/:id
+  def update?
+    owner?
+  end
+
   def to_moderate?
-    user.present? && record.user_id == user.id && record.may_to_moderate?
+    owner?
   end
 
   def archive?
-    user.present? && record.user_id == user.id && record.may_archive?
+    owner?
   end
 
-  def publish?
-    user&.admin? && record.may_publish?
-  end
+  private
 
-  def reject?
-    user&.admin? && record.may_reject?
+  def owner?
+    user.present? && record.user_id == user.id
   end
 end
